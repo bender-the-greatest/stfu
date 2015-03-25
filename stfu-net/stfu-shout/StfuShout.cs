6f045f1c81c8c163f3dc05021c186b24cf294597
Alexander Alber <alexa@danlawinc.com>
2015-03-24 20:45:14 -0400

    Added .NET projects. Description is as follows:
    stfu-listener: Windows' service that will listen for notifications from the notification pane or programs running in the taskbar, and push them from the guest.
    
    stfu-shout: Windows' service that will watch the Virtualbox guestproperties for notifications to display to the host.

---
+++ stfu-net/stfu-shout/StfuShout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stfu_shout
+{
+    public partial class StfuShout : ServiceBase
+    {
+        public StfuShout()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
